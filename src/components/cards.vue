fb6eb18b4354cfa9cc0d4b1f8051f303e8fe5faa
Jose <Jose>
2019-03-08 17:54:30 -0300

    new API data has been added

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <h1><u>{{cards.data.card.name}}</u></h1>
-    <h4><i>{{cards.data.card.flavor}}</i></h4>
+    <h2>{{cards.data.card.type}}</h2>
     <img v-bind:src="cards.data.card.imageUrl" width="300" alt="" class="card-image">
     <p>{{cards.data.card.text}}</p>
+    <h4><i>{{cards.data.card.flavor}}</i></h4>
   </div>
 </template>
 
@@ -18,7 +19,7 @@ export default {
   },
 
   mounted () {
-    axios.get('https://api.magicthegathering.io/v1/cards/130552').then(response => (this.cards = response))
+    axios.get('https://api.magicthegathering.io/v1/cards/129465').then(response => (this.cards = response))
   }
 }
 </script>
